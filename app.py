105afce81c875d44e5a64fc497017b2c416c1f00
nikitin_teacher <nikitin@gsom.spbu.ru>
2024-05-17 06:43:14 +0000

    have changed user to moderator

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/user")
+@app.route("/moderator")
 def hello_user():
-    return "Hello user"
+    return "Hello moderator"
 
 
 if __name__ == "__main__":
